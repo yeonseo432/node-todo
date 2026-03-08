d9763a401be5cb2f0b710cac8b881c3ae269a355
Yeonseo <yeonseo432@snu.ac.kr>
2026-03-09 02:01:54 +0900

    feat: id 추가, done, update, delete 기능 추가.
    고유한 id를 부여하기 위한 전역변수 같은 것이 없어서 별도의 json 파일을 만들어 id의 index처럼 활용하였다.
    const로 선언한 변수에 ++ 연산을 하려고 하니 에러가 발생해서 저장할 때 +1 연산을 하도록 했다.
    json은 문자열이기 때문에 수정한 부분만 따로 저장할 수 없고 전체를 다시 저장해야 한다는 것을 인지했다.
    터미널로 입력받은 것은 항상 문자열로 인식되기 때문에 숫자와 === 연산을 하기 위해서 Number() 함수를 사용했다.
    delete의 경우 객체를 지우는 게 아니라 리스트 전체를 filter로 다시 쓰는 방식이 표준이라는 것을 알게 되었다.

--- todo.js
+++ todo.js
@@ -4,23 +4,67 @@ const fileData = fs.readFileSync('todos.json', 'utf-8');
 const parsedData = JSON.parse(fileData);
 const todos = parsedData;
 
+const id = JSON.parse(fs.readFileSync('id.json', 'utf-8'));
+
 const type = process.argv[2];
 const content = process.argv[3];
 
 if ( type == "add" ) {
-    todos.push({ content: content, isFinished: false });
+    todos.push({ id: id, content: content, isFinished: false });
     const jsonData = JSON.stringify(todos, null, 2);
+    const jsonidData = JSON.stringify(id+1, null, 2);
     fs. writeFileSync('todos.json', jsonData);
+    fs. writeFileSync('id.json', jsonidData);
     console.log(`Todo가 추가되었습니다: ${content}`);
 }
 
 if ( type == `list` && todos.length > 0 ) {
     todos.map((todo, index) => {
         const status = todo.isFinished ? `[x]` : `[ ]`;
-        console.log(`${status} ${index + 1}. ${todo.content}`);
+        console.log(`${status} ${todo.id}. ${todo.content}`);
     })
 } else if ( type == `list` ) {
     console.log('Todo가 없습니다.');
+}
+
+if ( type == "done" ) {
+    const doneTodo = todos.find(todo => todo.id === Number(content));
+
+    if(doneTodo) {
+        doneTodo.isFinished = true;
+        console.log(`id ${content}번 항목이 완료되었습니다.`);
+        const jsonData = JSON.stringify(todos, null, 2);
+        fs. writeFileSync('todos.json', jsonData);
+    } else {
+        console.log(`해당 id를 찾을 수 없습니다.`);
+    }
+}
+
+if ( type == "update" ) {
+    const updatedTodo = todos.find(todo => todo.id === Number(content));
+
+    if(updatedTodo) {
+        const newContent = process.argv[4];
+        updatedTodo.content = newContent;
+        console.log(`id ${content}번 항목이 수정되었습니다: ${newContent}`);
+        const jsonData = JSON.stringify(todos, null, 2);
+        fs. writeFileSync('todos.json', jsonData);
+    } else {
+        console.log(`해당 id를 찾을 수 없습니다.`);
+    }
+}
+
+if ( type == "delete" ) {
+    const deletedTodo = todos.find(todo => todo.id === Number(content));
+
+    if(deletedTodo) {
+        const filteredTodos = todos.filter(todo => todo.id !== Number(content))
+        console.log(`id ${content}번 항목이 삭제되었습니다.`);
+        const jsonData = JSON.stringify(filteredTodos, null, 2);
+        fs. writeFileSync('todos.json', jsonData);
+    } else {
+        console.log(`해당 id를 찾을 수 없습니다.`);
+    }
 }
 
 console.log("실행 완료!");
